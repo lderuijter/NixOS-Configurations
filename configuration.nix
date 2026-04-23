bf13ac36ce44b176d49c7102fcd7e3f120f2220f
lderuijter <lucasder2003@gmail.com>
2026-04-23 23:18:42 +0200

    Switched to KDE Plasma 6 with X11 to get the Android Emulator to work on API 31, Android version 12 and Android Open Source API, no Google bloatware

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running 'nixos-help').
 
 { config, pkgs, inputs, ... }:
 
@@ -45,9 +45,13 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = false;
-  services.xserver.desktopManager.gnome.enable = false;
+  # KDE Plasma 6 on X11
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.lightdm.enable = false;
+  services.displayManager.defaultSession = "plasmax11";
+
+  services.displayManager.sddm.wayland.enable = false;  # disable wayland in sddm
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -66,106 +70,91 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rocco = {
     isNormalUser = true;
     description = "Rocco";
     extraGroups = [ "networkmanager" "wheel" "bluetooth" "libvirtd" "adbusers" "kvm" ];
     packages = with pkgs; [
-    #  thunderbird
     ];
   };
 
   nixpkgs.config.allowUnfree = true;
 
-  # Use Niri.
-  programs.niri.enable = true;
-
-  # Generic Linux binaries
+  # Generic Linux binaries (needed for Android Studio / emulator)
   programs.nix-ld.enable = true;
 
-  # Disable firefox
-  programs.firefox.enable = false;
-
-  # Virtualization for Android Studio
+  # Virtualization for Android Studio emulator
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
   services.flatpak.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-environment.systemPackages = with pkgs; [
-  # noctalia
-  inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-  # zen-browser
-  inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-  # terminal
-  kitty
-  wl-clipboard
-  xdg-utils
-  inotify-tools
-  python3
-  git
-  # screenshots
-  grim
-  slurp
-  # file manager
-  nautilus
-  # media/audio
-  playerctl
-  brightnessctl
-  wireplumber
-  # utils his scripts need
-  jq
-  curl
-  # custom cursor
-  phinger-cursors
-  # fetch
-  fastfetch
-  # tasks
-  btop
-  htop
-  # app store flatpak
-  gnome-software
-  # others
-  xhost
-  # ssl
-  openssl
-  # xwayland
-  xwayland-satellite
-  # android studio
-  android-studio
-  # adb
-  android-tools
-  # gsettings
-  gsettings-desktop-schemas
-  glib
-  # terminal theme
-  starship
-];
+  environment.systemPackages = with pkgs; [
+    # zen-browser
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
 
-programs.xwayland.enable = true;
+    # terminal
+    kitty
 
-environment.sessionVariables = {
-  XCURSOR_THEME = "phinger-cursors-dark";
-  XCURSOR_SIZE = "16";
-  NIXOS_OZONE_WL = "1";
-  _JAVA_AWT_WM_NONREPARENTING = "1";
-};
+    xclip
+    xsel
+
+    xdg-utils
+    inotify-tools
+    python3
+    git
+
+    kdePackages.spectacle   # KDE screenshot tool
+    scrot       # CLI fallback
+
+    # file manager
+    kdePackages.dolphin
+
+    # media/audio
+    playerctl
+    brightnessctl
+    wireplumber
+
+    # utils
+    jq
+    curl
+
+    # custom cursor
+    phinger-cursors
+
+    # fetch
+    fastfetch
+
+    # tasks
+    btop
+    htop
+
+    # app store / flatpak
+    gnome-software
+
+    # ssl
+    openssl
+
+    # android studio + tools
+    android-studio
+    android-tools
+
+    gsettings-desktop-schemas
+    glib
+
+    # terminal theme
+    starship
+  ];
+
+  environment.sessionVariables = {
+    XCURSOR_THEME = "phinger-cursors-dark";
+    XCURSOR_SIZE = "24";
+    ANDROID_EMULATOR_USE_SYSTEM_LIBS = "1";
+  };
 
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
@@ -179,12 +168,8 @@ environment.sessionVariables = {
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  
-  services.displayManager.sddm.enable = true;
 
   services.dbus.enable = true;
 
@@ -205,68 +190,54 @@ environment.sessionVariables = {
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  # Discord privacy
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
 
   programs.nix-ld.libraries = with pkgs; [
-  libX11
-  libXext
-  libXrender
-  libXtst
-  libXi
-  libXfixes
-  libXcursor
-  libXrandr
-  libpulseaudio
-  libGL
-  libpng
-  libxcb
-
-xcbutil
-xcbutilcursor
-xcbutilimage
-xcbutilkeysyms
-xcbutilrenderutil
-xcbutilwm
-
-  libxkbfile
-libXxf86vm
-libXdamage
-libXcomposite
-libXinerama
-freetype
-fontconfig
-cairo
-pango
-atk
-gtk2
-libsecret
-libbsd
-libunwind
-libgpg-error
-libgcrypt
-lz4
-zstd
-bzip2
-xz
-pcre2
-util-linux
-e2fsprogs
-  vulkan-loader
-  zlib
-  glib
-  gtk3
-  nss
-  nspr
-  dbus
-  expat
-  libdrm
-  mesa
-  alsa-lib
-  udev
-  systemd
-];
+    libX11
+    libXext
+    libXrender
+    libXtst
+    libXi
+    libXfixes
+    libXcursor
+    libXrandr
+    libXdamage
+    libXcomposite
+    libxcb
+    xcbutil
+    xcbutilcursor
+    xcbutilimage
+    xcbutilkeysyms
+    xcbutilrenderutil
+    xcbutilwm
+    libxkbfile
+    libGL
+    vulkan-loader
+    libdrm
+    mesa
+    libpulseaudio
+    alsa-lib
+    gtk3
+    cairo
+    pango
+    fontconfig
+    freetype
+    libpng
+    glib
+    dbus
+    expat
+    zlib
+    zstd
+    util-linux
+    libsecret
+    nss
+    stdenv.cc.cc.lib
+    sqlite
+    libunwind
+    udev
+    systemd
+  ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -276,10 +247,9 @@ e2fsprogs
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
--- flake.nix
+++ flake.nix
@@ -1,15 +1,8 @@
 {
   description = "NixOS config";
 
-  nixConfig = {
-    extra-substituters = [ "https://noctalia.cachix.org" ];
-    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    noctalia.url = "github:noctalia-dev/noctalia-shell";
-    noctalia.inputs.nixpkgs.follows = "nixpkgs";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
