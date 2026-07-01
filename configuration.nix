f04b7d361689c0bbd3b49302e131d94c6b3a41e4
lderuijter <lucasder2003@gmail.com>
2026-07-01 23:55:45 +0200

    explicit kernel upgrade to bleeding edge

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "NixOS"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
