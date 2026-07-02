bb833906bce4bdc99768b0bfc19d31f5553e07c7
lderuijter <lucasder2003@gmail.com>
2026-07-02 20:12:42 +0200

    add steam

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,18 @@
 
   services.flatpak.enable = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
