00c3b6132ad5ef5e45f8c15bff7c7c695f0426ec
lderuijter <lucasder2003@gmail.com>
2026-07-02 20:14:53 +0200

    remove duplicate line

--- configuration.nix
+++ configuration.nix
@@ -206,8 +206,6 @@
   boot.kernelModules = [ "btusb" "kvm-amd" ];
   boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];
 
-  hardware.graphics.enable = true;
-
   # Nvidia
   hardware.nvidia = {
     modesetting.enable = true;
