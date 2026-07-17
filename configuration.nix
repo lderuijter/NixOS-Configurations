682215a15e61e84f3b29283e41583cf4ea8e0151
lderuijter <lucasder2003@gmail.com>
2026-07-17 17:03:43 +0200

    removed nxapi from source, will do via npm instead (recommended

--- configuration.nix
+++ configuration.nix
@@ -130,9 +130,6 @@
     # zen-browser
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
 
-    # splatoon 3 status for discord
-    (callPackage ./nxapi.nix {})
-
     # terminal
     kitty
 
